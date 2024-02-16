676487ce133c9f0476870ab7b05c072f9a109b74
Ivan Camilo <buitragoivancamilo1025@gmail.com>
2024-02-16 16:51:09 -0500

    feat 🎉: IU for navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,27 @@
-function App() {
+import { Route, Routes } from 'react-router-dom'
+
+import { HomePage } from './pages/HomePaje'
+import { LoginPage } from './pages/LoginPage'
+import { SignUpPage } from './pages/SignUpPage'
+import { ExplorePage } from './pages/ExplorePage'
+import { LikesPage } from './pages/LikesPage'
+
+import { Sidebar } from "./components/Sidebar"
+
+export const App = () => {
 
   return (
-    <h1>Github Like App</h1>
+    <div className="flex">
+      <Sidebar />
+      <div className='max-w-5xl my-5 text-white mx-auto transition-all duration-300 flex-1'>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignUpPage />} />
+          <Route path="/explore" element={<ExplorePage />} />
+          <Route path="/likes" element={<LikesPage />} />
+        </Routes>
+      </div>
+    </div>
   )
 }
-
-export default App
---
+++ frontend/src/pages/LikesPage.jsx
@@ -0,0 +1,5 @@
+export const LikesPage = () => {
+  return (
+    <div>Likes Page</div>
+  )
+}

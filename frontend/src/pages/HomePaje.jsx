0e037d6df800d1c930883f811735fbd892cdc902
Ivan Camilo <buitragoivancamilo1025@gmail.com>
2024-02-19 06:51:35 -0500

    feat 🎉 IU for component Repos

--- frontend/src/pages/HomePaje.jsx
+++ frontend/src/pages/HomePaje.jsx
@@ -1,16 +1,16 @@
 import { Search } from "../components/Search"
 import { SortRepos } from "../components/SortRepos"
 import { ProfileInfo } from "../components/ProfileInfo"
-// import { Repos } from "../components/Repos"
+import { Repos } from "../components/Repos"
 
 export const HomePage = () => {
   return (
     <div className="m-4">
       <Search />
       <SortRepos />
-      <div className="flex gap-4 flex-col lg:flex-row justify-center items-center">
+      <div className="flex gap-4 flex-col lg:flex-row justify-center items-start">
         <ProfileInfo />
-        {/* <Repos /> */}
+        <Repos />
       </div>
     </div>
   )

39a36a7ecdff2b244748d582c8457acf7f07f2fe
Ivan Camilo <buitragoivancamilo1025@gmail.com>
2024-02-19 06:21:48 -0500

    feat 🎉 IU for component Search

---
+++ frontend/src/components/SortRepos.jsx
@@ -0,0 +1,5 @@
+export const SortRepos = () => {
+  return (
+    <div>Sort Repos</div>
+  )
+}
--- frontend/src/pages/HomePaje.jsx
+++ frontend/src/pages/HomePaje.jsx
@@ -1,5 +1,17 @@
+import { Search } from "../components/Search"
+// import { SortRepos } from "../components/SortRepos"
+// import { ProfileInfo } from "../components/ProfileInfo"
+// import { Repos } from "../components/Repos"
+
 export const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <div className="m-4">
+      <Search />
+      {/* <SortRepos /> */}
+      <div className="flex gap-4 flex-col lg:flex-row justify-center items-center">
+        {/* <ProfileInfo /> */}
+        {/* <Repos /> */}
+      </div>
+    </div>
   )
 }

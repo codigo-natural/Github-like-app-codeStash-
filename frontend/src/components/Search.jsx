0ea9cf271114f5a27ad13fe76a770b3be0eee0e3
Ivan Camilo <buitragoivancamilo1025@gmail.com>
2024-02-21 16:08:50 -0500

    feat 🎉 Search users

--- frontend/src/components/Search.jsx
+++ frontend/src/components/Search.jsx
@@ -1,9 +1,17 @@
+import { useState } from 'react'
 import { IoSearch } from 'react-icons/io5'
 
-export const Search = () => {
+export const Search = ({ onSearch }) => {
+  const [username, setUsername] = useState("");
   return (
-    <form className='max-w-xl mx-auto p-2 sm:w-72'>
-      <label htmlFor='default-search' className='mb-2 text-sm font-medium text-gray-900 sr-only'>
+    <form
+      onSubmit={(e) => onSearch(e, username)}
+      className='max-w-xl mx-auto p-2 sm:w-72'
+    >
+      <label
+        htmlFor='default-search'
+        className='mb-2 text-sm font-medium text-gray-900 sr-only'
+      >
         Search
       </label>
       <div className='relative'>
@@ -14,8 +22,10 @@ export const Search = () => {
           type='search'
           id='default-search'
           className='block w-full p-4 ps-10 text-sm rounded-lg bg-glass focus:ring-blue-500 focus:border-blue-500 bg-transparent focus:bg-transparent '
-          placeholder='i.e. johndoe'
+          placeholder='codigo-natural'
           required
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
         />
         <button
           type='submit'

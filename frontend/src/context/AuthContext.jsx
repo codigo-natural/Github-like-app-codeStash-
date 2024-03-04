efa612217451fe9b3a17b0ec4a2635094063d512
Ivan Camilo <buitragoivancamilo1025@gmail.com>
2024-03-03 20:09:37 -0500

    feat 🎉: implement loading state

---
+++ frontend/src/context/AuthContext.jsx
@@ -0,0 +1,39 @@
+import { createContext, useState, useEffect, useContext } from 'react'
+import { toast } from "react-hot-toast"
+
+export const AuthContext = createContext()
+
+// eslint-disable-next-line react-refresh/only-export-components
+export const useAuthContext = () => {
+  return useContext(AuthContext)
+}
+
+export const AuthContextProvider = ({ children }) => {
+  const [authUser, setAuthUser] = useState(null)
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    const checkUserLoggedIn = async () => {
+      setLoading(true)
+      try {
+        const response = await fetch("/api/auth/check", {
+          credentials: "include"
+        })
+        const data = await response.json()
+        setAuthUser(data.user)
+      } catch (error) {
+        toast.error(error.message)
+      } finally {
+        setLoading(false)
+      }
+    }
+    checkUserLoggedIn()
+  }, [])
+
+
+  return (
+    <AuthContext.Provider value={{ authUser, setAuthUser, loading }}>
+      {children}
+    </AuthContext.Provider>
+  )
+}
